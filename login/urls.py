f98e7003be5a04e2eb994d4ec13d107d05f36a23
Hai Sok <vanhai92.do@gmail.com>
2022-07-01 22:48:25 +0700

    run login app

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,6 @@ from .views import login, my_view, my_logout
 
 urlpatterns = [
     # path('', login, name="login_han"),
-    # path('accounts/login/', my_view, name="login_handler"),
+    path('accounts/login/', my_view, name="login_handler"),
     # path('logout', my_logout, name="logout_handler"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 # def login(request):
@@ -7,16 +8,16 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def my_view(request):
+    # next_page = request.GET.get('next')
+    # print(next_page)
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        next_page = request.GET.get('next')
         if user is not None:
             login(request, user)
-            if next_page:
-                return redirect(next_page)
-
+            # if next_page:
+                # return HttpResponseRedirect(request.GET['next'])
             return redirect('home')
         else:
             return render(request, 'login/login.html')
